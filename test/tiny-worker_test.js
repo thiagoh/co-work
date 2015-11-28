26e023381d8fc1ab8d128e4df4600309ad1f4216
Thiago Andrade <thiagoh@gmail.com>
2015-11-28 07:54:06 -0300

    grunt

---
+++ test/tiny-worker_test.js
@@ -0,0 +1,147 @@
+'use strict';
+
+/*
+  ======== A Handy Little Nodeunit Reference ========
+  https://github.com/caolan/nodeunit
+
+  Test methods:
+    test.expect(numAssertions)
+    test.done()
+  Test assertions:
+    test.ok(value, [message])
+    test.equal(actual, expected, [message])
+    test.notEqual(actual, expected, [message])
+    test.deepEqual(actual, expected, [message])
+    test.notDeepEqual(actual, expected, [message])
+    test.strictEqual(actual, expected, [message])
+    test.notStrictEqual(actual, expected, [message])
+    test.throws(block, [error], [message])
+    test.doesNotThrow(block, [error], [message])
+    test.ifError(value)
+*/
+
+var tinyWorker = require('../lib/tiny-worker.js'),
+  Q = require('q'),
+  sleep = function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e10; i++) {
+      if ((new Date().getTime() - start) > milliseconds) {
+        break;
+      }
+    }
+  },
+  times = function(n, command) {
+    var accum = [];
+    for (var i = 0; i < n; i++) {
+      accum.push(command(i));
+    }
+    return accum;
+  };
+
+var test_work_x_commands_async = function(test, max_workers_same_time, count_workers_to_be_executed) {
+
+  var count_workers_to_be_executed_test = count_workers_to_be_executed,
+    count_workers_same_time = 0,
+    getCommand = function() {
+
+      return function() {
+
+        var deferred = Q.defer(),
+          promise = deferred.promise;
+
+        ++count_workers_same_time;
+
+        setTimeout(function() {
+          test.ok(count_workers_same_time <= max_workers_same_time, "More workers than permitted. " + count_workers_same_time + " workers are running when should be only " + max_workers_same_time);
+          sleep(5);
+          deferred.resolve(--count_workers_same_time);
+          if (--count_workers_to_be_executed_test === 0) {
+            test.done();
+          }
+        }, 10);
+
+        return promise;
+      };
+    },
+    commands = times(count_workers_to_be_executed, getCommand);
+
+  test.ok(commands.length === count_workers_to_be_executed, "It should exist " + count_workers_to_be_executed + " commands");
+  tinyWorker.work(max_workers_same_time, commands);
+  test.expect(count_workers_to_be_executed + 1);
+  if (count_workers_to_be_executed_test === 0) {
+    test.done();
+  }
+};
+
+// module.exports.testSomething = function(test) {
+// test.expect(1);
+// test.ok(true, "this assertion should pass");
+// test.done();
+// };
+// 
+// module.exports.testSomethingElse = function(test) {
+// test.ok(false, "this assertion should fail");
+// test.done();
+// };
+
+
+exports['work'] = {
+  setUp: function(done) {
+    // setup here
+    done();
+  },
+  'test_work_1000_commands_sync': function(test) {
+
+    var max_workers_same_time = 100,
+      count_workers_to_be_executed = 1000,
+      count_workers_to_be_executed_test = count_workers_to_be_executed,
+      count_workers_same_time = 0,
+      getCommand = function() {
+
+        return function() {
+
+          ++count_workers_same_time;
+
+          test.ok(count_workers_same_time <= max_workers_same_time, "More workers than permitted. " + count_workers_same_time + " workers are running when should be only " + max_workers_same_time);
+          sleep(5);
+          --count_workers_same_time;
+          if (--count_workers_to_be_executed_test === 0) {
+            test.done();
+          }
+        };
+      },
+      commands = times(count_workers_to_be_executed, getCommand);
+
+    test.ok(commands.length === count_workers_to_be_executed, "It should exist " + count_workers_to_be_executed + " commands");
+    tinyWorker.work(max_workers_same_time, commands);
+    test.expect(count_workers_to_be_executed + 1);
+  },
+  'test_work_0_commands_async': function(test) {
+
+    var max_workers_same_time = 100,
+      count_workers_to_be_executed = 0;
+
+    test_work_x_commands_async(test, max_workers_same_time, count_workers_to_be_executed);
+  },
+  'test_work_99_commands_async': function(test) {
+
+    var max_workers_same_time = 100,
+      count_workers_to_be_executed = 99;
+
+    test_work_x_commands_async(test, max_workers_same_time, count_workers_to_be_executed);
+  },
+  'test_work_100_commands_async': function(test) {
+
+    var max_workers_same_time = 100,
+      count_workers_to_be_executed = 100;
+
+    test_work_x_commands_async(test, max_workers_same_time, count_workers_to_be_executed);
+  },
+  'test_work_1000_commands_async': function(test) {
+
+    var max_workers_same_time = 100,
+      count_workers_to_be_executed = 1000;
+
+    test_work_x_commands_async(test, max_workers_same_time, count_workers_to_be_executed);
+  }
+};
