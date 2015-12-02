2ae53859c9dc2a94897d81198635aee61357b24d
Thiago Andrade <thiagoh@gmail.com>
2015-12-02 16:40:46 -0300

    tests

--- dist/co-work.js
+++ dist/co-work.js
@@ -1,4 +1,4 @@
-/*! co-work - v0.1.0 - 2015-12-02
+/*! co-work - v0.1.8 - 2015-12-02
 * https://github.com/thiagoh/co-work
 * Copyright (c) 2015 Thiago Andrade; Licensed MIT */
 (function(exports) {
